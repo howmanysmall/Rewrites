86923d78af2c9064cdd5a4ede28bff0eadf30578
howmanysmall <lhamil29@bruinmail.slcc.edu>
2019-11-27 19:01:46 -0700

    Update BindableEvent.lua

--- src/Utility/BindableEvent.lua
+++ src/Utility/BindableEvent.lua
@@ -9,10 +9,19 @@ BindableEvent.__index = BindableEvent
 
 local ipairs = ipairs
 
+--[[**
+	Creates a new BindableEvent.
+	@returns [BindableEvent]
+**--]]
 function BindableEvent.new()
 	return setmetatable({}, BindableEvent)
 end
 
+--[[**
+	Fires every function in the BindableEvent with the given varargs.
+	@param [t:any] ... The arguments you are firing with.
+	@returns [void]
+**--]]
 function BindableEvent:Fire(...)
 	for _, Function in ipairs(self) do
 		local Thread = coroutine.create(Function)
@@ -20,10 +29,20 @@ function BindableEvent:Fire(...)
 	end
 end
 
+--[[**
+	Connects the given function to the BindableEvent.
+	@param [t:callback] Function The function you are connecting.
+	@returns [void]
+**--]]
 function BindableEvent:Connect(Function)
 	self[#self + 1] = Function
 end
 
+--[[**
+	Disconnects the given function from the BindableEvent.
+	@param [t:callback] Function The function you are removing.
+	@returns [void]
+**--]]
 function BindableEvent:Disconnect(Function)
 	local Length = #self
 
@@ -36,6 +55,10 @@ function BindableEvent:Disconnect(Function)
 	end
 end
 
+--[[**
+	Yields the current thread until the BindableEvent is fired.
+	@returns [t:any] Whatever the BindableEvent was fired with.
+**--]]
 function BindableEvent:Wait()
 	local Thread = coroutine.running()
 
@@ -57,6 +80,10 @@ function BindableEvent:Wait()
 	return coroutine.yield()
 end
 
+--[[**
+	Destroys the BindableEvent object.
+	@returns [void]
+**--]]
 function BindableEvent:Destroy()
 	for Index in ipairs(self) do
 		self[Index] = nil
