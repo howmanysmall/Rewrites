ac79c71378edbd54eca5fb253ab234bb75d56f71
howmanysmall <lhamil29@bruinmail.slcc.edu>
2019-12-27 10:22:31 -0700

    Create Libraries.lua
    fresh

---
+++ src/Libraries/Libraries.lua
@@ -0,0 +1,342 @@
+return {
+	Janitor = {
+		URL = "https://raw.githubusercontent.com/RoStrap/Events/master/Janitor.lua";
+		Documentation = "https://rostrap.github.io/Libraries/Events/Janitor/";
+		ParentFolderPath = "Events";
+	};
+
+	Signal = {
+		URL = "https://raw.githubusercontent.com/RoStrap/Events/master/Signal.lua";
+		Documentation = "https://rostrap.github.io/Libraries/Events/Signal/";
+		ParentFolderPath = "Events";
+	};
+
+	ReplicatedValue = {
+		URL = "https://raw.githubusercontent.com/RoStrap/Events/master/ReplicatedValue.lua";
+		Documentation = "https://rostrap.github.io/Libraries/Events/ReplicatedValue/";
+		ParentFolderPath = "Events";
+	};
+
+	Keys = {
+		URL = "https://raw.githubusercontent.com/RoStrap/Input/master/Keys.lua";
+		Documentation = "https://rostrap.github.io/Libraries/Input/Keys/";
+		ParentFolderPath = "Input";
+	};
+
+	Enumeration = {
+		URL = "https://raw.githubusercontent.com/RoStrap/Classes/master/Enumeration.lua";
+		Documentation = "https://rostrap.github.io/Libraries/Classes/Enumeration";
+		ParentFolderPath = "Classes";
+	};
+
+	PseudoInstance = {
+		URL = "https://raw.githubusercontent.com/RoStrap/Classes/master/PseudoInstance.lua";
+		Documentation = "https://rostrap.github.io/Libraries/Classes/PseudoInstance/";
+		ParentFolderPath = "Classes";
+	};
+
+	ReplicatedPseudoInstance = {
+		URL = "https://raw.githubusercontent.com/RoStrap/Classes/master/ReplicatedPseudoInstance.lua";
+		Documentation = "https://rostrap.github.io/Libraries/Classes/ReplicatedPseudoInstance/";
+		ParentFolderPath = "Classes";
+	};
+
+	Sentry = {
+		URL = "https://github.com/RoStrap/Sentry";
+		Documentation = "https://rostrap.github.io/Libraries/Sentry/";
+	};
+
+	Bezier = {
+		URL = "https://raw.githubusercontent.com/RoStrap/Interpolation/master/Bezier.lua";
+		Documentation = "https://rostrap.github.io/Libraries/Interpolation/Bezier/";
+		ParentFolderPath = "Interpolation";
+	};
+
+	EasingFunctions = {
+		URL = "https://raw.githubusercontent.com/RoStrap/Interpolation/master/EasingFunctions.lua";
+		Documentation = "https://rostrap.github.io/Libraries/Interpolation/EasingFunctions/";
+		ParentFolderPath = "Interpolation";
+	};
+
+	Lerps = {
+		URL = "https://raw.githubusercontent.com/RoStrap/Interpolation/master/Lerps.lua";
+		Documentation = "https://rostrap.github.io/Libraries/Interpolation/Lerps/";
+		ParentFolderPath = "Interpolation";
+	};
+
+	Tween = {
+		URL = "https://raw.githubusercontent.com/RoStrap/Interpolation/master/Tween.lua";
+		Documentation = "https://rostrap.github.io/Libraries/Interpolation/Tween/";
+		ParentFolderPath = "Interpolation";
+	};
+
+	Array = {
+		URL = "https://raw.githubusercontent.com/RoStrap/DataTypes/master/Array.lua";
+		Documentation = "https://rostrap.github.io/Libraries/DataTypes/Array/";
+		ParentFolderPath = "DataTypes";
+	};
+	--[[
+	HTMLParser = {
+		URL = "https://raw.githubusercontent.com/RoStrap/DataTypes/master/HTMLParser.lua";
+		Documentation = "https://rostrap.github.io/Libraries/DataTypes/HTMLParser/";
+		ParentFolderPath = "DataTypes";
+	};
+	--]]
+	SortedArray = {
+		URL = "https://raw.githubusercontent.com/RoStrap/DataTypes/master/SortedArray.lua";
+		Documentation = "https://rostrap.github.io/Libraries/DataTypes/SortedArray/";
+		ParentFolderPath = "DataTypes";
+	};
+
+	Table = {
+		URL = "https://raw.githubusercontent.com/RoStrap/DataTypes/master/Table.lua";
+		Documentation = "https://rostrap.github.io/Libraries/DataTypes/Table/";
+		ParentFolderPath = "DataTypes";
+	};
+
+	BigNum = {
+		URL = "https://raw.githubusercontent.com/RoStrap/Math/master/BigNum.lua";
+		Documentation = "https://rostrap.github.io/Libraries/Math/BigNum/";
+		ParentFolderPath = "Math";
+	};
+
+	Leveler = {
+		URL = "https://raw.githubusercontent.com/RoStrap/Math/master/Leveler.lua";
+		Documentation = "https://rostrap.github.io/Libraries/Math/Leveler/";
+		ParentFolderPath = "Math";
+	};
+
+	Normal = {
+		URL = "https://raw.githubusercontent.com/RoStrap/Math/master/Normal.lua";
+		Documentation = "https://rostrap.github.io/Libraries/Math/Normal/";
+		ParentFolderPath = "Math";
+	};
+
+	Spring = {
+		URL = "https://raw.githubusercontent.com/RoStrap/Math/master/Spring.lua";
+		Documentation = "https://rostrap.github.io/Libraries/Math/Spring/";
+		ParentFolderPath = "Math";
+	};
+
+	WeightedProbabilityFunction = {
+		URL = "https://raw.githubusercontent.com/RoStrap/Math/master/WeightedProbabilityFunction.lua";
+		Documentation = "https://rostrap.github.io/Libraries/Math/WeightedProbabilityFunction/";
+		ParentFolderPath = "Math";
+	};
+
+	FastSpawn = {
+		URL = "https://raw.githubusercontent.com/RoStrap/Helper/master/FastSpawn.lua";
+		Documentation = "https://rostrap.github.io/Libraries/Helper/FastSpawn/";
+		ParentFolderPath = "Helper";
+	};
+
+	Make = {
+		URL = "https://raw.githubusercontent.com/RoStrap/Helper/master/Make.lua";
+		Documentation = "https://rostrap.github.io/Libraries/Helper/Make/";
+		ParentFolderPath = "Helper";
+	};
+
+	Date = {
+		URL = "https://raw.githubusercontent.com/RoStrap/Time/master/Date.lua";
+		Documentation = "https://rostrap.github.io/Libraries/Time/Date/";
+		ParentFolderPath = "Time";
+	};
+
+	SyncedPoller = {
+		URL = "https://raw.githubusercontent.com/RoStrap/Time/master/SyncedPoller.lua";
+		Documentation = "https://rostrap.github.io/Libraries/Time/SyncedPoller/";
+		ParentFolderPath = "Time";
+	};
+
+	ConditionalPoller = {
+		URL = "https://raw.githubusercontent.com/RoStrap/Time/master/ConditionalPoller.lua";
+		Documentation = "https://rostrap.github.io/Libraries/Time/ConditionalPoller/";
+		ParentFolderPath = "Time";
+	};
+
+	Debug = {
+		URL = "https://raw.githubusercontent.com/RoStrap/Debugging/master/Debug.lua";
+		Documentation = "https://rostrap.github.io/Libraries/Debugging/Debug/";
+		ParentFolderPath = "Debugging";
+	};
+
+	Typer = {
+		URL = "https://raw.githubusercontent.com/RoStrap/Debugging/master/Typer.lua";
+		Documentation = "https://rostrap.github.io/Libraries/Debugging/Typer/";
+		ParentFolderPath = "Debugging";
+	};
+
+	AsymmetricTransformation = {
+		URL = "https://raw.githubusercontent.com/RoStrap/RoStrapUI/master/AsymmetricTransformation.lua";
+		Documentation = "https://rostrap.github.io/Libraries/RoStrapUI/AsymmetricTransformation/";
+		ParentFolderPath = "RoStrapUI";
+	};
+
+	Checkbox = {
+		URL = "https://raw.githubusercontent.com/RoStrap/RoStrapUI/master/Checkbox.lua";
+		Documentation = "https://rostrap.github.io/Libraries/RoStrapUI/Checkbox/";
+		ParentFolderPath = "RoStrapUI";
+	};
+
+	ChoiceDialog = {
+		URL = "https://raw.githubusercontent.com/RoStrap/RoStrapUI/master/ChoiceDialog.lua";
+		Documentation = "https://rostrap.github.io/Libraries/RoStrapUI/ChoiceDialog/";
+		ParentFolderPath = "RoStrapUI";
+	};
+
+	Color = {
+		URL = "https://raw.githubusercontent.com/RoStrap/RoStrapUI/master/Color.lua";
+		Documentation = "https://rostrap.github.io/Libraries/RoStrapUI/Color/";
+		ParentFolderPath = "RoStrapUI";
+	};
+
+	Radio = {
+		URL = "https://raw.githubusercontent.com/RoStrap/RoStrapUI/master/Radio.lua";
+		Documentation = "https://rostrap.github.io/Libraries/RoStrapUI/Radio/";
+		ParentFolderPath = "RoStrapUI";
+	};
+
+	RadioGroup = {
+		URL = "https://raw.githubusercontent.com/RoStrap/RoStrapUI/master/RadioGroup.lua";
+		Documentation = "https://rostrap.github.io/Libraries/RoStrapUI/RadioGroup/";
+		ParentFolderPath = "RoStrapUI";
+	};
+
+	RippleButton = {
+		URL = "https://raw.githubusercontent.com/RoStrap/RoStrapUI/master/RippleButton.lua";
+		Documentation = "https://rostrap.github.io/Libraries/RoStrapUI/RippleButton/";
+		ParentFolderPath = "RoStrapUI";
+	};
+
+	Rippler = {
+		URL = "https://raw.githubusercontent.com/RoStrap/RoStrapUI/master/Rippler.lua";
+		Documentation = "https://rostrap.github.io/Libraries/RoStrapUI/Rippler/";
+		ParentFolderPath = "RoStrapUI";
+	};
+
+	RoStrapPriorityUI = {
+		URL = "https://raw.githubusercontent.com/RoStrap/RoStrapUI/master/RoStrapPriorityUI.lua";
+		Documentation = "WIP";
+		ParentFolderPath = "RoStrapUI";
+	};
+
+	SelectionController = {
+		URL = "https://raw.githubusercontent.com/RoStrap/RoStrapUI/master/SelectionController.lua";
+		Documentation = "https://rostrap.github.io/Libraries/RoStrapUI/SelectionController/";
+		ParentFolderPath = "RoStrapUI";
+	};
+
+	Shadow = {
+		URL = "https://raw.githubusercontent.com/RoStrap/RoStrapUI/master/Shadow.lua";
+		Documentation = "https://rostrap.github.io/Libraries/RoStrapUI/Shadow/";
+		ParentFolderPath = "RoStrapUI";
+	};
+
+	Snackbar = {
+		URL = "https://raw.githubusercontent.com/RoStrap/RoStrapUI/master/Snackbar.lua";
+		Documentation = "https://rostrap.github.io/Libraries/RoStrapUI/Snackbar/";
+		ParentFolderPath = "RoStrapUI";
+	};
+
+	Try = {
+		URL = "Validark/RBX-Try-Library/tree/patch-4";
+		Description = "An asynchronous pcall-wrapper for interdependent error-prone functions";
+	};
+
+	EvLightning = {
+		URL = "evaera/EvLightning";
+		Description = "Realistic-looking lightning bolt generator";
+		Documentation = "https://github.com/evaera/EvLightning/blob/master/README.md";
+	};
+
+	RadialImage = {
+		URL = "evaera/RadialSpriteSheetGenerator/blob/master/roblox/RadialImage.lua";
+		Description = "A library which displays radial progress indicators using a sprite sheet generated by a nifty tool";
+		Documentation = "https://github.com/evaera/RadialSpriteSheetGenerator/blob/master/README.md";
+	};
+
+	Ready = {
+		URL = "EmeraldSlash/RbxReady/blob/master/Ready.lua";
+		Description = "A Library for yielding until an object's descendants have finished replicating";
+		Documentation = "https://github.com/EmeraldSlash/RbxReady/blob/master/README.md";
+	};
+
+	DataStoreService = {
+		URL = "buildthomas/MockDataStoreService/tree/master/lib";
+		Description = "An implementation of DataStoreService in Lua for use in Studio";
+		Documentation = "https://github.com/buildthomas/MockDataStoreService/blob/master/README.md";
+	};
+
+	Promise = {
+		URL = "evaera/roblox-lua-promise/tree/master/lib";
+		Description = "An implementation of Promise similar to Promise/A+";
+		Documentation = "https://eryn.io/roblox-lua-promise/";
+	};
+
+	Cmdr = {
+		URL = "evaera/Cmdr";
+		Description = "A fully extensible and type-safe admin-commands console";
+		Documentation = "https://github.com/evaera/Cmdr/blob/master/README.md";
+		ParentFolderPath = "Cmdr.Server"; -- Dot separated directory within Repository.Packages into which Cmdr will be installed
+	};
+
+	Aurora = {
+		URL = "evaera/Aurora/tree/master/lib";
+		Description = "A library that can manage status effects (known as \"Auras\") in your Roblox game";
+		Documentation = "https://github.com/evaera/Aurora/blob/master/README.md";
+	};
+
+	Roact = {
+		URL = "Roblox/roact/tree/master/src";
+		Description = "A declarative UI library similar to Facebook's React";
+		Documentation = "https://roblox.github.io/roact/";
+	};
+
+	Rodux = {
+		URL = "Roblox/rodux/tree/master/lib";
+		Description = "A state management library inspired by Redux";
+		Documentation = "https://roblox.github.io/rodux/";
+	};
+
+	["Roact-Rodux"] = {
+		URL = "Roblox/roact-rodux/tree/master/lib";
+		Description = "An ergonomic binding between Roact and Rodux";
+		Documentation = "https://roblox.github.io/roact-rodux/";
+	};
+
+	Replica = {
+		URL = "headjoe3/Replica/tree/master/Replica";
+		Description = "An easily controllable Server/Client data replication library";
+		Documentation = "https://github.com/headjoe3/Replica/blob/master/README.md";
+	};
+
+	Rocrastinate = {
+		URL = "headjoe3/Rocrastinate/tree/master/Rocrastinate";
+		Description = "A React/Redux-like state management and view library optimized for Roblox";
+		Documentation = "https://github.com/headjoe3/Rocrastinate/blob/master/docs/1-1-introduction.md";
+	};
+
+	DataStore2 = {
+		URL = "Kampfkarren/Roblox/tree/master/DataStore2";
+		Description = "DataStore2 is a simple to use data store system that caches, leading to significantly faster performance over traditional data stores";
+		Documentation = "https://kampfkarren.github.io/Roblox/";
+	};
+
+	ContextControls = {
+		URL = "vocksel/ContextControls/tree/master/lib";
+		Description = "Wrapper for ContextActionService with easy mobile button support";
+		Documentation = "https://github.com/vocksel/ContextControls/blob/master/README.md";
+	};
+
+	t = {
+		URL = "osyrisrblx/t/tree/master/lib";
+		Description = "A module which allows you to create type definitions to check values against";
+		Documentation = "https://github.com/osyrisrblx/t/blob/master/README.md";
+	};
+
+	Tag = {
+		URL = "vocksel/Tag/tree/master/lib";
+		Description = "Class to make tagging instances easy and typesafe";
+		Documentation = "https://github.com/vocksel/Tag/blob/master/README.md";
+	};
+}
